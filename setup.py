4834341ae6ce633d94ade18fb34db4136c846bcf
adam-mehdi <62762546+adam-mehdi@users.noreply.github.com>
2021-06-15 06:53:22 -0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,12 +4,14 @@ from setuptools import setup, find_packages
 
 setup(
     name='MuarAugment',
-    version='1.0',
+    version='1.0.1',
     description='State-of-the-art data augmentation search algorithms in PyTorch',
+    long_description=README,
+    long_description_content_type="text/markdown",
     author='Adam Mehdi',
     author_email='amehdi.25@dartmouth.edu',
     url='https://github.com/adam-mehdi/MuarAugment',
     install_requires=['pytorch-lightning', 'kornia'],
-    packages=find_packages(),
+    packages=find_packages(where='muar'),
 )
 
